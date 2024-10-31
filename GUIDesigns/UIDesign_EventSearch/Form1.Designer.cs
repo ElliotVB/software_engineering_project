3c22ecd4e31dc87ebec832b4ab316eb963314fb3
Elliot Brown <elliot.vdbb@gmail.com>
2024-10-31 15:14:15 +0000

    Created new project for WPF design

--- GUIDesigns/UIDesign_EventSearch/Form1.Designer.cs
+++ GUIDesigns/UIDesign_EventSearch/Form1.Designer.cs
@@ -48,6 +48,7 @@
             this.button8 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.navigationPanel.SuspendLayout();
             this.calendarsPanel.SuspendLayout();
             this.eventsPanel.SuspendLayout();
@@ -371,11 +372,19 @@
             this.button9.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
             this.button9.UseVisualStyleBackColor = true;
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(216, 87);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(574, 23);
+            this.textBox1.TabIndex = 3;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.eventsPanel);
             this.Controls.Add(this.calendarsPanel);
             this.Controls.Add(this.navigationPanel);
@@ -386,6 +395,7 @@
             this.calendarsPanel.ResumeLayout(false);
             this.eventsPanel.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -411,5 +421,6 @@
         private Button button8;
         private Button button7;
         private Button button9;
+        private TextBox textBox1;
     }
 }
