bec765d50bfa17a1c04c83e86ac30fbe0d841d93
Elliot Brown <elliot.vdbb@gmail.com>
2024-10-31 16:37:23 +0000

    Updated WPF event search UI

--- GUIDesigns/UIDesign_EventSearchWPF/MainWindow.xaml.cs
+++ GUIDesigns/UIDesign_EventSearchWPF/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace UIDesign_EventSearchWPF
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
