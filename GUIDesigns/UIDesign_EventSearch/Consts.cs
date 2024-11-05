e2e76fad3bef7abc5a4ff7ab3c8ae3edef23398f
Elliot Brown <elliot.vdbb@gmail.com>
2024-11-05 17:21:27 +0000

    Continued work on database implementation

---
+++ GUIDesigns/UIDesign_EventSearch/Consts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UIDesign_EventSearch
+{
+    //All global consts are stored here
+    internal class Consts
+    {
+
+        public static string SELECTALL = "SELECT * FROM Event";
+
+    }
+}
---
+++ GUIDesigns/UIDesign_EventSearch/DBConnectionClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using Microsoft.Data.SqlClient;
+
+namespace UIDesign_EventSearch
+{
+    //Creates a connection to the database
+    internal class DBConnectionClass
+    {
+        //Private object of the class itself
+        private static DBConnectionClass _instance;
+
+        //Connection string
+        private string connStr;
+
+        //Connection to the DB
+        private SqlConnection connToDB;
+
+        //Constructor
+        private DBConnectionClass()
+        {
+            connStr = Properties.Settings.Default.eventDatabaseConnString;
+        }
+
+
+        //METHODS
+
+        //Static method which creates a unique object of the class itself
+        public static DBConnectionClass getInstanceOfDBConnection()
+        {
+            //Only creates object if it doesn't already exist
+            if (_instance == null)
+                _instance = new DBConnectionClass();
+            return _instance;
+        }
+
+        //Returns a data set built based on the query sent as parameter
+        public DataSet
+    }
+}
