8d7696f29aeebb6aab9f09c33c1819e5ce42adbd
azizjon0 <aa3070@student.aru.ac.uk>
2024-12-01 18:35:41 +0000

    Add files via upload

---
+++ GUIDesigns/UIDesign_MainPage_and_Profile/Profile_Mainmenu/Profile_Mainmenu/Form1.cs
@@ -0,0 +1,25 @@
+namespace Profile_Mainmenu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
