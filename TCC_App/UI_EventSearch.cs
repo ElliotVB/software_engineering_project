ba75262e94d8460e34a3f8c4dbb55ff0f4dbfe29
Elliot Brown <elliot.vdbb@gmail.com>
2024-12-02 12:50:59 +0000

    Added search functionality for event view

--- TCC_App/UI_EventSearch.cs
+++ TCC_App/UI_EventSearch.cs
@@ -24,40 +24,81 @@ namespace TCC_App
     public partial class UI_EventSearch : UserControl
     {
         private FormUI form;
-
         public UI_EventSearch(FormUI form)
         {
             InitializeComponent();
             this.form = form;
+
+            //This is done in code since the foreColour is used in searchBox_Clicked()
+            searchBox.ForeColor = global.DefaultSearchBoxColour;
 
             GenerateEventsFromDB();
         }
 
 
         //Generates a set of events within the event layout
+        //Only shows events which contin the searchTerm (which is empty by default, resulting in all events being shown)
         //Currently just generates "dummy" events - will eventually be changed to generate events from the DB
-        private void GenerateEventsFromDB()
+        private void GenerateEventsFromDB(string searchTerm = "")
         {
+            //Clears events from layout if any exist
+            if (eventFlowLayout.Controls.Count > 0)
+            {
+                eventFlowLayout.Controls.Clear();
+            }
+
+
 
             //Tgenerates 20 "dummy" events
             for (int i = 0; i < 20; i++)
             {
                 //CREATE A NEW CLASS THAT INHERITS FROM BUTTON (to link to event view page)
                 //also give the text a border in that class's draw function
-                Button b = new EventButton(form, "Event no. " + (i + 1), "Example event description.", "TCC building", "N/A", "N/A", 5, 3, 0, i);
-                eventFlowLayout.Controls.Add(b);
+                EventButton b = new EventButton(form, "Event no. " + (i + 1), "Example event description.", "TCC building", "N/A", "N/A", 5, 3, 0, i);
+
+                //Checks if event matches search term
+                //TEMP - this check should be called when the EventButton, not when it is added to the flow layout. It's here for now until the DB is implemented.
+
+                if (global.CheckSearchTerm(b.GetInfoForSearch(), searchTerm))
+                {
+                    eventFlowLayout.Controls.Add(b);
+                }
             }
         }
 
+        //Deletes the default text in the search box the first time it's clicked
         private void searchBox_Clicked(object sender, EventArgs e)
         {
-            //Deletes the default text in the search box the first time it's clicked
-            if (searchBox.ForeColor == Color.Gray)
+            if (!IsSearchDefaultTextClear())
             {
                 searchBox.Text = null;
                 searchBox.ForeColor = Color.Black;
             }
         }
+
+        //Updates the events display to only show events which contain the search term in their name, description, or tags
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            //Only runs search if the default text has been cleared
+            if (IsSearchDefaultTextClear())
+            {
+                GenerateEventsFromDB(searchBox.Text);
+            }
+        }
+
+        //Returns true if the default text has been cleared, otherwise returns false
+        private bool IsSearchDefaultTextClear()
+        {
+            if (searchBox.ForeColor == global.DefaultSearchBoxColour)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
 
         private void navigationPanel_Paint(object sender, PaintEventArgs e)
         {
